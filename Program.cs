623391cc1409c117608f0d7ff20e0668089ca1f8
Che Sergiu <37384588+CHESERGIU@users.noreply.github.com>
2020-07-03 10:47:48 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace App31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numbers = Console.ReadLine();
+            char number = Console.ReadLine()[0];
+
+            bool found = false;
+
+            for (int i = 0; i < numbers.Length; i++)
+                if (numbers[i] == number) found = true;
+
+            Console.WriteLine(found);
+        }
+    }
+}
